f8e2fca81851fecdf46f9ed921774fcf78eedc20
Isaac Verminck <isaacverm@gmail.com>
2016-12-22 22:47:43 +0100

    Imported overview top 999 questions all time

--- documentation/links.md
+++ documentation/links.md
@@ -1,1 +1,7 @@
+# Steps
+
 http://t-redactyl.io/blog/2015/11/analysing-reddit-data-part-2-extracting-the-data.html
+
+# 1000 posts limit
+
+https://www.reddit.com/r/redditdev/comments/2zxglv/best_way_to_download_comments_from_a_subreddit/
---
+++ scripts/import/import_top_questions_alltime.R
@@ -0,0 +1,90 @@
+### preliminary
+
+## packages
+
+library(jsonlite)
+library(data.table)
+
+## remarks
+
+### import top questions
+
+## function to get top questions
+
+get_top_questions <- function(after) {
+  
+                      # base url
+  
+                      base_url <- "https://www.reddit.com/r/AskHistorians/top/.json?sort=top&t=all&limit=100&after="
+                      
+                      # get json
+                      
+                      questions <- fromJSON(paste0(base_url,after))
+                      
+                      # get last id
+                      
+                      last_question_ind <- nrow(questions[["data"]][["children"]])
+
+                      last_id <- questions[["data"]][["children"]][["data"]][["name"]][last_question_ind]
+                      
+                      # return
+                      
+                      result <- list(questions,last_id)
+                      names(result) <- c("questions","last_id")
+                      
+                      return(result) }
+
+## import
+
+questions <- get_top_questions(after = "")
+
+nr_comments <- 1000
+
+while(length(questions) < nr_comments/50)  {
+  
+            Sys.sleep(3)
+  
+            additional_questions <- get_top_questions(after = questions[[length(questions)]])
+  
+            questions <- c(questions, additional_questions) }
+
+### clean top questions
+
+## remove last ids
+
+questions <- questions[-grep(pattern = "last_id", x = names(questions))]
+
+## information available
+
+names(questions[[1]][["data"]][["children"]][["data"]])
+
+## extract interesting information
+
+# interesting variables
+
+questions[[1]][["data"]][["children"]][["data"]][["ups"]]
+
+interesting_var <- c("selftext","gilded","author","score","over_18","edited","link_flair_css_class",
+                     "author_flair_css_class","archived","url","author_flair_text","title","link_flair_text",
+                     "num_comments","ups")
+
+# extract
+
+interesting_info <- lapply(questions,
+                           function(x) {
+                                        df <- as.data.frame(lapply(interesting_var,
+                                                                   function(y) x[["data"]][["children"]][["data"]][[y]]))
+                                        names(df) <- interesting_var
+                                        
+                                        return(df)
+                                        }
+                           )
+
+## bind as dataframe
+
+top_questions <- rbindlist(interesting_info)
+
+### save
+
+save(top_questions, file = file.path(base,"output","data","top_questions_alltime.RData"))
+
